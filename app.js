c756412a97a79f1bbe71eded26c4c5abe5145bb1
Kevin Rodríguez <desarrollo01@designplus.co>
2018-12-05 15:59:07 -0500

    Final manifest.json

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () =>
+    navigator.serviceWorker.register('service-worker.js')
+    .then(registration => console.log('Service Worker registered'))
+    .catch(err => 'SW registration failed'));
+}
+
 document.getElementById('photograph').onchange = function (e) {
   var file = e.target.files[0]
   if (file && file.name) {
