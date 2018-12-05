347a5eb57868d0e6a5d3979c080547f67c7234ab
Kevin Rodríguez <desarrollo01@designplus.co>
2018-12-05 15:17:13 -0500

    Basic styling

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ document.getElementById('photograph').onchange = function (e) {
       document.getElementById('image').src = image.result
     }
     EXIF.getData(file, function () {
+      var button = document.getElementById('copy')
       var result = document.getElementById('result')
       var exifData = EXIF.pretty(this)
       if (exifData) {
-        result.innerHTML = exifData
+        var datetime = EXIF.getTag(this, 'DateTime')
+        var iso = EXIF.getTag(this, 'ISOSpeedRatings')
+        var make = EXIF.getTag(this, 'Make')
+        var model = EXIF.getTag(this, 'Model')
+        var focalLength = EXIF.getTag(this, 'FocalLength')
+        var shutterSpeed = EXIF.getTag(this, 'ShutterSpeedValue')
+        var flash = EXIF.getTag(this, 'Flash')
+        var meteringMode = EXIF.getTag(this, 'MeteringMode')
+        result.innerHTML = `Date & Time: ${datetime}\n`
+        result.innerHTML += `Camera: ${make} ${model}\n`
+        result.innerHTML += `ISO: ${iso}\n`
+        result.innerHTML += `Focal-Length: ${focalLength}\n`
+        result.innerHTML += `Shutter Speed: ${shutterSpeed}\n`
+        result.innerHTML += `Flash: ${flash}\n`
+        result.innerHTML += `Metering Mode: ${meteringMode}\n`
+        button.style.display = "block"
       } else {
         result.innerHTML = 'No EXIF data found in your image'
+        button.style.display = "none"
       }
     })
   }
